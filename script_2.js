412c0ada0d5f727d730af707d9e592f9c8ef482c
Anna-gif-ua <anna.gadiyeva@gmail.com>
2020-05-30 07:10:12 +0300

    added files

--- script_2.js
+++ script_2.js
@@ -38,25 +38,4 @@
 
  })()
 
-// function getSymbol (s) {
 
-//     if ( Boolean(s)) {
-//         switch (s) {
-//             case '0': return `There is ")" under the number ${s}.`;
-//             case '1': return `There is "!" under the number ${s}.`;
-//             case '2': return `There is "@" under the number ${s}.`;
-//             case '3': return `There is "#" under the number ${s}.`;
-//             case '4': return `There is "$" under the number ${s}.`;
-//             case '5': return `There is "%" under the number ${s}.`;
-//             case '6': return `There is "^" under the number ${s}.`;
-//             case '7': return `There is "&" under the number ${s}.`;
-//             case '8': return `There is "*" under the number ${s}.`;
-//             case '9': return `There is "(" under the number ${s}.`;
-//             default: return "I don't know this!";
-//         }
-//     } else if ( s == '') {
-//         return (getSymbol(prompt("You forgot enter a number! Try again! \n \n What number of the symbol you want me to return?")));
-//     } else {
-//         return ("Goodbye!");
-//     }
-// }
